8b4b4ee4e60a004d508f67dbf121c4d13e0dc584
Alassane Watt <lastaw@alassanes-mbp.home>
2020-10-25 18:07:46 +0100

    Updates

---
+++ precache-manifest.dbfdeb4774594dbb4c636cf303d5241e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a5220055383ab064a082e247f8cf29",
+    "url": "/portfolio-react-app/index.html"
+  },
+  {
+    "revision": "918e5ee52bea1ac1b5c0",
+    "url": "/portfolio-react-app/static/css/main.920e3b4f.chunk.css"
+  },
+  {
+    "revision": "85dd529527a18929554f",
+    "url": "/portfolio-react-app/static/js/2.65aa3cfb.chunk.js"
+  },
+  {
+    "revision": "918e5ee52bea1ac1b5c0",
+    "url": "/portfolio-react-app/static/js/main.f6b7c376.chunk.js"
+  },
+  {
+    "revision": "3e8ff0f32ed20f68884e",
+    "url": "/portfolio-react-app/static/js/runtime-main.6ae928e3.js"
+  }
+]);
